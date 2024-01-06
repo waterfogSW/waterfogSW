c2c063535dc2fc8ae58a1289c633f8e489edabff
San Kim <waterfog9580@gmail.com>
2024-01-06 14:19:13 +0900

    Update and rename main.py to readme.py

--- readme.py
+++ readme.py
@@ -1,11 +1,11 @@
 import feedparser, time
 
-URL = "https://waterfogsw.tistory.com/rss"
+URL="https://waterfogsw.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
 
 markdown_text = """
-## ✅ Latest Blog Post
+## Latest Blog Post
 
 """  # list of blog posts will be appended here
 
@@ -14,7 +14,7 @@ for idx, feed in enumerate(RSS_FEED['entries']):
         break
     else:
         feed_date = feed['published_parsed']
-        markdown_text += f"- [{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
         
 f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
