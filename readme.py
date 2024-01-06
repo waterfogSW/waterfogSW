f11f02b018dc7e3e6b40e75cd12976a03d8b16c4
San Kim <waterfog9580@gmail.com>
2024-01-06 14:49:57 +0900

    Update readme.py

--- readme.py
+++ readme.py
@@ -1,21 +1,26 @@
 import feedparser, time
 
-URL="https://waterfogsw.tistory.com/rss"
+URL = "https://waterfogsw.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
 
 markdown_text = """
-## Latest Blog Post
+**Interested In**
+- Java, Kotlin, Spring, MySQL
+- DDD, Hexagonal Architecture, Classicist TDD
+- FastAPI, Python, Open AI, VectorDB
+
+**Latest Blog Post**
 
 """  # list of blog posts will be appended here
 
 for idx, feed in enumerate(RSS_FEED['entries']):
     if idx > MAX_POST:
         break
-    else:
+    if 'category' in feed:  # Check if the 'category' tag exists
         feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-        
+        markdown_text += f"- [{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+
 f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
 f.close()
